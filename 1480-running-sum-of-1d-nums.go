3ef1b16a277ada72f5e74b725c215ae01b20b31b
hitzhangjie <hit.zhangjie@gmail.com>
2020-06-28 17:39:09 +0800

    sum of 1d nums

--- 1480-running-sum-of-1d-nums.go
+++ 1480-running-sum-of-1d-nums.go
@@ -1,1 +1,16 @@
 package go_leetcode
+
+func runningSum(nums []int) []int {
+	if len(nums) == 0 {
+		return nil
+	}
+	sums := make([]int, len(nums))
+	for i := 0; i < len(nums); i++ {
+		if i == 0 {
+			sums[i] = nums[i]
+		} else {
+			sums[i] = nums[i] + sums[i-1]
+		}
+	}
+	return sums
+}
