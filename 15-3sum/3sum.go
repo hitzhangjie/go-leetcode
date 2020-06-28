4ff377cea702eb5f9df81335bf4e21071c4c1975
hitzhangjie <hit.zhangjie@gmail.com>
2020-06-28 14:28:40 +0800

    3sum

--- 15-3sum/3sum.go
+++ 15-3sum/3sum.go
@@ -1,23 +1,47 @@
 package _5_3sum
 
-import "testing"
-
-func Test3Sum(t *testing.T) {
-
-}
+import (
+	"sort"
+)
 
 func threeSum(nums []int) [][]int {
+
 	results := [][]int{}
+
+	// 首先要排下序，就变成有序数组r、l指针扫描问题了
+	sort.Ints(nums)
+
 	for i := 0; i < len(nums)-2; i++ {
-		for j := i + 1; j < len(nums)-1; j++ {
-			for k := j + 1; k < len(nums); k++ {
-				if nums[i]+nums[j]+nums[k] == 0 {
-					result := []int{i, j, k}
-					results = append(results, result)
+
+		// nums [i]> nums[i-1]过滤掉重复搜索的情况
+		if i == 0 || nums[i] > nums[i-1] {
+
+			for l, r := i+1, len(nums)-1; l < r; {
+
+				if nums[l]+nums[r] == -1*nums[i] {
+					// 找到一个匹配，记录并调整搜索的左右边界
+					results = append(results, []int{nums[i], nums[l], nums[r]})
+					l++
+					r--
+					// 跳过相同的
+					for nums[l] == nums[l-1] {
+						l++
+					}
+					// 跳过相同的
+					for nums[r] == nums[r+1] {
+						r--
+					}
+				} else if nums[l]+nums[r] > -1*nums[i] {
+					// 调整右边界
+					r--
+				} else {
+					// 调整左边界
+					l++
 				}
 			}
 		}
 	}
+
 	if len(results) != 0 {
 		return results
 	}
--- 15-3sum/3sum_test.go
+++ 15-3sum/3sum_test.go
@@ -1,1 +1,46 @@
 package _5_3sum
+
+import (
+	"fmt"
+	"reflect"
+	"testing"
+)
+
+func Test_threeSum(t *testing.T) {
+	type args struct {
+		nums []int
+	}
+	tests := []struct {
+		name string
+		args args
+		want [][]int
+	}{
+		{
+			"case-1",
+			args{[]int{-1, 0, 1, 2, -1, -4}},
+			[][]int{
+				{-1, 0, 1},
+				{-1, -1, 2},
+			},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got := threeSum(tt.args.nums)
+
+			m1 := map[string]struct{}{}
+			for _, v := range got {
+				m1[fmt.Sprintf("%d%d%d", v[0], v[1], v[2])] = struct{}{}
+			}
+
+			m2 := map[string]struct{}{}
+			for _, v := range tt.want {
+				m2[fmt.Sprintf("%d%d%d", v[0], v[1], v[2])] = struct{}{}
+			}
+
+			if !reflect.DeepEqual(m1, m2) {
+				t.Errorf("threeSum() = %v, want %v", m1, m2)
+			}
+		})
+	}
+}
