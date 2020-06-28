a30c160abc2ca2432a73c0626aa37450284987c6
hitzhangjie <hit.zhangjie@gmail.com>
2020-06-28 17:38:47 +0800

    3sum closest

--- 16-3sum-closest.go
+++ 16-3sum-closest.go
@@ -1,1 +1,49 @@
 package go_leetcode
+
+import (
+	"math"
+	"sort"
+)
+
+func threeSumClosest(nums []int, target int) int {
+
+	sort.Ints(nums)
+
+	var (
+		val     int = math.MaxInt64
+		minDiff int = math.MaxInt64
+	)
+
+	for i := 0; i < len(nums); i++ {
+
+		for l, r := i+1, len(nums)-1; l < r; {
+
+			sum := nums[i] + nums[l] + nums[r]
+			diff := sum - target
+			if diff < 0 {
+				diff = -1 * diff
+			}
+
+			if diff < minDiff {
+				minDiff = diff
+				val = sum
+			}
+
+			if sum == target {
+				return val
+			} else if sum > target {
+				r--
+				for r > l && nums[r] == nums[r+1] {
+					r--
+				}
+			} else {
+				l++
+				for l < r && nums[l] == nums[l-1] {
+					l++
+				}
+			}
+
+		}
+	}
+	return val
+}
--- 16-3sum-closest_test.go
+++ 16-3sum-closest_test.go
@@ -12,7 +12,21 @@ func Test_threeSumClosest(t *testing.T) {
 		args args
 		want int
 	}{
-		// TODO: Add test cases.
+		{
+			"case-1",
+			args{
+				[]int{-1, 2, 1, -4},
+				1,
+			},
+			2,
+		//}, {
+		//	"case-2",
+		//	args{
+		//		[]int{0, 1, 2},
+		//		3,
+		//	},
+		//	3,
+		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
